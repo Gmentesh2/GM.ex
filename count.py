b20ff3c1304d6adb0b2174e960ab2628f7c599d0
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2021-11-05 03:07:51 -0700

    initial commit

--- count.py
+++ count.py
@@ -1,32 +1,32 @@
-# for number in range(1,21):
-#     print(number)
+for number in range(1,21):
+    print(number)
 
-# for number in range(1,1000001):
-#     print(number)
+for number in range(1,1000001):
+    print(number)
 
-# lst = list(range(1,1000001))
-# print(sum(lst))
+lst = list(range(1,1000001))
+print(sum(lst))
 
-# odd_numbers = list(range(2,12,3))
-# print(odd_numbers)
+odd_numbers = list(range(2,12,3))
+print(odd_numbers)
 
-# for number in range(2,29,2):
-#     print(number)
+for number in range(2,29,2):
+    print(number)
 
-# for number in range(4,55,4):
-#     print(number)
+for number in range(4,55,4):
+    print(number)
 
 
 
-# cubes = []
+cubes = []
 
-# for number in range(1,11):
-#     cube = number ** 3 
-#     cubes.append(cube)
-# print(cubes)
+for number in range(1,11):
+    cube = number ** 3 
+    cubes.append(cube)
+print(cubes)
 
-# cubes = [number **3 for number in range(1,21)]
-# print(cubes)
+cubes = [number **3 for number in range(1,21)]
+print(cubes)
 
 
    
