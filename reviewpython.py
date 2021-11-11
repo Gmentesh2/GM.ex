4ea658a74eaca32617962569cb0fd42aff7a4ee5
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2021-11-11 08:09:42 -0700

    Review

--- reviewpython.py
+++ reviewpython.py
@@ -14,19 +14,19 @@
 # print(last_bike)
 # for bike in bikes:
 #     print(bikes)
-#adding items in list 
+# adding items in list 
 # bikes = []
 # bikes.append("trek")
 # bikes.append("redline")
 # bikes.append("giant")
 # print(bikes)
-#numerical lists
-squares = []
+# # numerical lists
+# squares = []
 # for x in range(1,11):
 #     squares.append(x**2)
 #     print(squares)
 
-## slicing a list
+# # slicing a list
 
 # list = ["bob","maks","lucas"]
 # first_two = list[0:2]
@@ -42,10 +42,74 @@ squares = []
 # elif age <18:
 #     print("ticket cost is 5$")
 # elif age >18:
-#     print("ticket cost is 10$")
+#     print("ticket cost is 10$
 
+# current_value = 1 
+# while current_value <= 5: 
+#     print(current_value) 
+#     current_value += 1
+# current_value1 = 2 
+# while current_value1 <=10:
+#     print(current_value1)
+#     current_value1 += 2
 
+# users = ['val', 'bob', 'mia', 'ron', 'ned']
+# print(users)
+# first_user = users[0]
+# second_user = users[1]
+# newest_user = users[-1]
+# print(first_user)
+# print(second_user)
+# print(newest_user)
 
+# users[0] = 'vlad'
+# print(users)
+# users[-2] = 'stanislav'
+# print(users)
+# users.append('giuli')
+# print(users)
+
+# people = []
+# people.append('maka')
+# people.append('naka')
+# people.append('kato')
+# print(people)
+# people.insert(1,'tekla')
+# people.insert(0,'liza')
+# print(people)
+# del people[0]
+# print(people)
+# people.remove('naka')
+# print(people)
+# most_recent_user = people.pop()
+# print(most_recent_user)
+# first_person = people.pop(0)
+# print(first_person)
+
+# users2 = ['val', 'bob', 'mia', 'ron', 'ned']
+# num_users2 = len(users2)
+# print("We have " + str(num_users2) + " users.")
+# print(num_users2)
+
+# users2.sort()
+# print(users2)
+# users2.sort(reverse=True)
+# print(users2)
+# users2.sort(reverse=False)
+# print(users2)
+
+# print(sorted(users2))
+# print(sorted(users2, reverse=True))
+# users2.reverse()
+# print(users2)
+
+# users3 = ['val', 'bob', 'mia', 'ron', 'ned']
+# for user in users3:
+#     print( user)
+# for user in users3:
+#      print("Welcome, " + user + "!")
+
+# print("\nWelcome we are glad to see you all " + "!")
 
 
 
